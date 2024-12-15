3ac190ed5b9785e2931eff3491369c975e3c2a34
Ramses Ariel <ramses.lopez2800@gmail.com>
2024-12-15 09:56:06 -0700

    Stock logic is now handled on createJob()

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import authRoutes from './auth.js'
 import jobsRoutes from './jobs.js'
 import engravablesRoutes from './engravables.js'
 import salesRepsRoutes from './salesReps.js'
-import stockHandler from './stockHandler.js'
 
 const router = Router()
 
@@ -11,6 +10,5 @@ router.use('/auth', authRoutes)
 router.use('/jobs', jobsRoutes)
 router.use('/engravables', engravablesRoutes)
 router.use('/sales_reps', salesRepsRoutes)
-router.use('/stock', stockHandler)
 
 export { router }
